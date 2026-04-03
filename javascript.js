71acacf58cf22419611a75a684d49aa333522162
Dylan M <dmurayama93@gmail.com>
2026-04-03 11:07:16 -1000

    add pseudocode

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const gridContainer = document.querySelector(".grid-container");
+
+// create a function that creates a box
+// height/width of box = width of container / x number of boxes
+// create a function that sets box color on hover
+
+// create a function that creates a row of boxes
+// create a function that creates multiple rows
+
+
