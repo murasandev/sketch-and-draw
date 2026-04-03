f780b3aa7946c6fc96ca15db7730b8c6f3943b74
Dylan M <dmurayama93@gmail.com>
2026-04-03 11:20:20 -1000

    create function to create a row of squares

--- javascript.js
+++ javascript.js
@@ -7,4 +7,21 @@ const gridContainer = document.querySelector(".grid-container");
 // create a function that creates a row of boxes
 // create a function that creates multiple rows
 
+function createBox(row){
+  
+}
+createRow();
 
+
+function createRow(){
+  const rowContainer = document.createElement("div");
+  rowContainer.classList.add("row-container");
+
+  for (let index = 0; index < 10; index++) {
+    const coloredSquare = document.createElement("div");
+    coloredSquare.classList.add("colored-square");
+    rowContainer.appendChild(coloredSquare);
+  }
+
+  gridContainer.appendChild(rowContainer);
+}
